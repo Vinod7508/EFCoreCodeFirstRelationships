8c3b3e8710dfec94e31c9948ed37f002075ecef6
vinod patil <vinodptl02@gmail.com>
2021-11-24 19:46:06 +0000

    EFcore code first migration part 1

---
+++ CodeFirstDatabaseMigration/Entities/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirstDatabaseMigration.Entities
+{
+
+    //The context class is another important part of the application.
+    //This class must inherit from the DbContext base class which contains information and configuration for accessing the database
+    public class ApplicationContext : DbContext
+    {
+        //Our ApplicationContext class currently accepts one parameter of type DbContextOptions inside a constructor.
+        //But we can provide the generic version of the DbContextOptions parameter as well:
+
+        //public ApplicationContext(DbContextOptions options) : base(options)
+        //{ }
+
+        //generic version
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        { }
+
+        //EF Core looks for all the public DbSet properties, inside the application’s context class, and then maps their names to the names of the tables in the database
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
---
+++ CodeFirstDatabaseMigration/Entities/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirstDatabaseMigration.Entities
+{
+    public class Student
+    {
+        //The model (entity) class is a class that Entity Framework Core uses for mapping to the database table. 
+        //efcore naming convention...StudentIDd is primary key here
+        public Guid StudentId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
