bbd13949e0f855c2d148ff56889140c5f24fcf21
vinod patil <vinodptl02@gmail.com>
2021-11-26 13:06:40 +0000

    configuration for data seeding as soon as application start

---
+++ Entities/MigrationManager.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+//this part is quite hard to understand....refer codemaze document given in below link
+//https://code-maze.com/migrations-and-seed-data-efcore/
+
+namespace Entities
+{
+  public static class MigrationManager
+    {
+        public static IHost MigrateDatabase(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                using (var appContext = scope.ServiceProvider.GetRequiredService<ApplicationContext>())
+                {
+                    try
+                    {
+                        appContext.Database.Migrate();
+                    }
+                    catch (Exception ex)
+                    {
+                        //Log errors or do anything you think it's needed
+                        throw;
+                    }
+                }
+            }
+            return host;
+        }
+    }
+}
